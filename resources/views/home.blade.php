65ce97ebdcf5609d2a608fa670c873c91b0e0488
Fabiopanetta02 <panetta.fabio20@gmail.com>
2022-09-19 13:59:39 +0200

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.main')
+
+@section('title', 'Home')
+
+@section('main-content')
+<h1>{{ $title }}</h1>
+<p>
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis molestiae optio atque mollitia, veritatis nisi similique magnam tempore.
+</p>
+
+@endsection
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My Movies | @yield('title', 'The best Movies on web')</title>
+
+    @yield('cdns')
+
+    {{-- style --}}
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+
+    {{-- JS--}}
+    <script defer src="{{ asset('js/app.js') }}"></script>
+</head>
+<body>
+    @include('includes.header')
+    <main>
+        <div class="container">
+            @yield('main-content')
+        </div>
+    </main>
+</body>
+</html>
