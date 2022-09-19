1a23d651a06ccc533b65da8b7890146f1347acb5
Fabiopanetta02 <panetta.fabio20@gmail.com>
2022-09-19 16:14:06 +0200

    Added fixed error and complete base exercise

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,11 +3,11 @@
 @section('title', 'Home')
 
 @section('main-content')
-<h1>{{ env('APP_NAME') }}</h1>
+<h1 class="mb-3">Movies</h1>
 <section id="movies">
     <div class="d-flex flex-wrap">
         @forelse ($movies as $movie)   
-            <div class="card" style="width: 18rem;">
+            <div class="card mb-3 mx-3" style="width: 18rem;">
                 <div class="card-body">
                 <h5 class="card-title">{{$movie->title}}</h5>
                 <h6 class="card-subtitle mb-2 text-muted">{{$movie->original_title}}</h6>
@@ -21,5 +21,4 @@
         @endforelse
     </div>
 </section>
-
 @endsection
